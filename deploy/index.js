7e0b172edb6e267972b2ed45201edebe622aadfc
fetorres <fetorres@groupon.com>
2016-10-11 23:53:02 -0300

    update

--- deploy/index.js
+++ deploy/index.js
@@ -16,7 +16,7 @@ plan.target('production', [
 // run commands on localhost
 plan.local((local) => {
   local.log('Copy files to remote hosts');
-  const filesToCopy = [`${process.env()}/deploy.tar`];
+  const filesToCopy = [`${process.cwd()}/deploy.tar`];
   // rsync files to all the target's remote hosts
   local.transfer(filesToCopy, `/tmp/${tmpDir}`);
 });
